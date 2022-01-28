aa26819b91052595b8a6ebbce1ac298ab46d4fb7
FHU-yezi <yehaowei20060411@qq.com>
2022-01-28 21:31:44 +0800

    docs：修复一处注释错误

--- data_getter.py
+++ data_getter.py
@@ -100,7 +100,7 @@ def GetWordcloud(articles_list: List[str], user_slug: str) -> None:
         words_count += Counter(cutted_text)
     wordcloud = WordCloud(font_path="wordcloud_assets/font.otf", width=1280, height=720,
                           background_color="white", max_words=100)
-    # 筛选出现五次以上的词
+    # 筛选出现十次以上的词
     img = wordcloud.generate_from_frequencies({key: value for key, value in words_count.items() if value > 10})
     img.to_file(f"user_data/{user_slug}/wordcloud_{user_slug}.png")
 
